996a88acd0e8143d0e7f0b88207117c3731cee10
Mohamed Atef <proofesser@gmail.com>
2022-09-08 09:16:24 +0200

    changes in layout fragment_search.xml

--- app/src/main/java/mo/atef/swensonhe/weatherapp/fragments/SearchViewFragment.kt
+++ app/src/main/java/mo/atef/swensonhe/weatherapp/fragments/SearchViewFragment.kt
@@ -1,10 +1,16 @@
 package mo.atef.swensonhe.weatherapp.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import mo.atef.swensonhe.weatherapp.R
@@ -16,6 +22,18 @@ import mo.atef.swensonhe.weatherapp.view.MainActivity
 class SearchViewFragment: Fragment() {
     lateinit var binding: FragmentSearchBinding
     lateinit var onArrowPressed:OnArrowPressed
+
+    lateinit var listAdapter: ArrayAdapter<String>
+    lateinit var suggestedList: ArrayList<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        suggestedList= ArrayList()
+        suggestedList.add("See")
+        suggestedList.add("Mohamed")
+        suggestedList.add("Mohsen")
+        suggestedList.add("Momken")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +47,23 @@ class SearchViewFragment: Fragment() {
         binding.arrowIcon.setOnClickListener { view:View->
             onArrowPressed.showAppBar()
         }
+        populateListView()
+        binding.editTextSearchBox.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.i("Search","BeforeText is called")
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.i("Search","onText is called")
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i("Search","afterText is called")
+                listAdapter.filter.filter(p0)
+            }
+        })
+
+
 
         return binding.root
     }
@@ -36,5 +71,10 @@ class SearchViewFragment: Fragment() {
     fun newInstance(mainActivity: MainActivity): Fragment? {
         Util.Companion.MainActivity=mainActivity
         return SearchViewFragment()
+    }
+
+    fun populateListView(){
+        listAdapter = ArrayAdapter(requireActivity(),android.R.layout.simple_list_item_1,suggestedList)
+        binding.listView.adapter=listAdapter
     }
 }
