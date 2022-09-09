81abca3bd49e89bdbbdebe69e80c43fc63421313
Mohamed Atef <proofesser@gmail.com>
2022-09-09 11:59:16 +0200

    layout edits - REST call edits-3

--- app/src/main/java/mo/atef/swensonhe/weatherapp/models/Forecast.kt
+++ app/src/main/java/mo/atef/swensonhe/weatherapp/models/Forecast.kt
@@ -1,5 +1,8 @@
 package mo.atef.swensonhe.weatherapp.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Forecast(
+    @SerializedName("forecastday")
     val forecastday: List<Forecastday>
 )
--- app/src/main/java/mo/atef/swensonhe/weatherapp/models/WeatherModel.kt
+++ app/src/main/java/mo/atef/swensonhe/weatherapp/models/WeatherModel.kt
@@ -1,7 +1,10 @@
 package mo.atef.swensonhe.weatherapp.models
 
+import com.google.gson.annotations.SerializedName
+
 data class WeatherModel(
     val current: Current,
+    @SerializedName("forecast")
     val forecast: Forecast,
     val location: Location
 )
